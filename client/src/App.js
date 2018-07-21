6cc781979750c9c677094f0965317b2dcffcdc9a
gracemiller23 <gracemiller0323@gmail.com>
2018-07-21 17:25:04 -0500

    moved router to header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,65 +1,68 @@
 import React, { Component } from 'react';
+import Header from './Components/Header';
 
-import {Router, Route, Redirect} from "react-router-dom";
+// import {Router, Route, Redirect} from "react-router-dom";
 
-import SubDashboard from './pages/SubDashboard';
-import Profileform from './pages/Profileform';
-import Callback from './pages/Callback';
-import NeedAdminApproval from './pages/NeedAdminApproval';
-import Landing from './pages/Landing';
+// import SubDashboard from './pages/SubDashboard';
+// import Profileform from './pages/Profileform';
+// import Callback from './pages/Callback';
+// import NeedAdminApproval from './pages/NeedAdminApproval';
+// import Landing from './pages/Landing';
 
-import Auth from './Auth/Auth';
-import history from './history';
+// import Auth from './Auth/Auth';
+// import history from './history';
 
-const auth = new Auth();
+// const auth = new Auth();
 
-const handleAuthentication = (nextState, replace) => {
-  if (/access_token|id_token|error/.test(nextState.location.hash)) {
-    auth.handleAuthentication();
-  }
-}
+// const handleAuthentication = (nextState, replace) => {
+//   if (/access_token|id_token|error/.test(nextState.location.hash)) {
+//     auth.handleAuthentication();
+//   }
+// }
 
 class App extends Component {
 
   render() {
-    const {isAuthenticated} = auth;
+    // const {isAuthenticated} = auth;
 
     return (
-      <Router history={history}>
-      <div >
-        <div>
-          {
-            isAuthenticated() ? 
-            (<div>Logged In <button onClick={() => auth.logout()}>Log Out</button></div>) 
-            : 
-            (<div>Logged Out <button onClick={() => auth.login()}>Log In</button></div>)
-          }
-          </div>
+      <Header />
 
-        <Route exact path="/" render={(props)=><Landing auth={auth} {...props}/>}/>
-        <Route exact path="/awaitapproval" render={(props)=><NeedAdminApproval auth={auth} {...props}/>}/>
+//       <Router history={history}>
+//       <div >
+//         <div>
+//           {
+//             isAuthenticated() ? 
+//             (<div>Logged In <button onClick={() => auth.logout()}>Log Out</button></div>) 
+//             : 
+//             (<div>Logged Out <button onClick={() => auth.login()}>Log In</button></div>)
+//           }
+//           </div>
+
+//         <Route exact path="/" render={(props)=><Landing auth={auth} {...props}/>}/>
+//         <Route exact path="/awaitapproval" render={(props)=><NeedAdminApproval auth={auth} {...props}/>}/>
         
-        <Route exact path="/buildprofile" render={(props)=><Profileform auth={auth} {...props}/>}/>
-        <Route exact path="/subdashboard" render={(props)=><SubDashboard auth={auth} {...props}/>}/>
+//         <Route exact path="/buildprofile" render={(props)=><Profileform auth={auth} {...props}/>}/>
+//         <Route exact path="/subdashboard" render={(props)=><SubDashboard auth={auth} {...props}/>}/>
 
-        <Route exact path="/editsubprofile" render={(props)=>{
-        const isAllowed = auth.isAuthenticated() && auth.userHasScopes(['write:projects']);
-        return  isAllowed ?
-                (<Profileform auth={auth} {...props}/>)
-                :(
-                  <Redirect to="/"/>
-                )
-              }
-      }/>
+//         <Route exact path="/editsubprofile" render={(props)=>{
+//         const isAllowed = auth.isAuthenticated() && auth.userHasScopes(['write:projects']);
+//         return  isAllowed ?
+//                 (<Profileform auth={auth} {...props}/>)
+//                 :(
+//                   <Redirect to="/"/>
+//                 )
+//               }
+//       }/>
 
- <Route path="/callback" render={(props) => {
-          handleAuthentication(props);
-          return <Callback {...props} /> 
-        }}/>
+//  <Route path="/callback" render={(props) => {
+//           handleAuthentication(props);
+//           return <Callback {...props} /> 
+//         }}/>
 
         
-      </div>
-      </Router>
+//       </div>
+//       </Router>
     );
   }
 }
--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,4 +1,66 @@
 import React, { Component } from 'react';
 import {Router, Route, Redirect} from "react-router-dom";
 
+import SubDashboard from '../pages/SubDashboard';
+import Profileform from '../pages/Profileform';
+import Callback from '../pages/Callback';
+import NeedAdminApproval from '../pages/NeedAdminApproval';
+import Landing from '../pages/Landing';
 
+import Auth from '../Auth/Auth';
+import history from '../history';
+
+const auth = new Auth();
+
+const handleAuthentication = (nextState, replace) => {
+  if (/access_token|id_token|error/.test(nextState.location.hash)) {
+    auth.handleAuthentication();
+  }
+}
+
+class Header extends Component {
+
+    render() {
+      const {isAuthenticated} = auth;
+  
+      return (
+        <Router history={history}>
+        <div >
+          <div>
+            {
+              isAuthenticated() ? 
+              (<div>Logged In <button onClick={() => auth.logout()}>Log Out</button></div>) 
+              : 
+              (<div>Logged Out <button onClick={() => auth.login()}>Log In</button></div>)
+            }
+            </div>
+  
+          <Route exact path="/" render={(props)=><Landing auth={auth} {...props}/>}/>
+          <Route exact path="/awaitapproval" render={(props)=><NeedAdminApproval auth={auth} {...props}/>}/>
+          
+          <Route exact path="/buildprofile" render={(props)=><Profileform auth={auth} {...props}/>}/>
+          <Route exact path="/subdashboard" render={(props)=><SubDashboard auth={auth} {...props}/>}/>
+  
+          <Route exact path="/editsubprofile" render={(props)=>{
+          const isAllowed = auth.isAuthenticated() && auth.userHasScopes(['write:projects']);
+          return  isAllowed ?
+                  (<Profileform auth={auth} {...props}/>)
+                  :(
+                    <Redirect to="/"/>
+                  )
+                }
+        }/>
+  
+   <Route path="/callback" render={(props) => {
+            handleAuthentication(props);
+            return <Callback {...props} /> 
+          }}/>
+  
+          
+        </div>
+        </Router>
+      );
+    }
+  }
+  
+  export default Header;
